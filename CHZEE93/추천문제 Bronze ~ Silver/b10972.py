a813a2a2e6df7c978cfb8eaed9efbf60c149c1ca
JeongSeokHyeon <128110499+CHZEE93@users.noreply.github.com>
2025-01-16 21:41:45 +0900

    백준 10972번 (다음 순열)

---
+++ CHZEE93/추천문제 Bronze ~ Silver/b10972.py
@@ -0,0 +1,24 @@
+#백준 10972번 (다음 순열)
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+len_arr = len(arr)
+
+i = len_arr - 1
+while i > 0 and arr[i-1] >= arr[i]:
+    i -= 1
+
+if i == 0:
+    print(-1)
+else:
+    j = len_arr - 1
+    while arr[j] <= arr[i-1]:
+        j -= 1
+
+    arr[i-1], arr[j] = arr[j], arr[i-1]
+
+    arr[i:] = reversed(arr[i:])
+
+    print(*arr)
+
