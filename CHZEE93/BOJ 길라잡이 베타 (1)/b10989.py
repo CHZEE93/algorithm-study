981a2984a78d36a4f07093817f847888c0c99bea
JeongSeokHyeon <128110499+CHZEE93@users.noreply.github.com>
2025-02-05 21:38:11 +0900

    백준 10989번 (수 정렬하기3)
    계수 정렬
    1.추가적인 리스트 생성이 없음 → 메모리를 최소한으로 사용 (O(N))
    2.연산 속도가 빠름 → O(N + K), 여기서 K=10000 이므로 O(N) 수준3.의 정렬 속도
    3.입력값 범위가 제한적 (1~10000)이라서 최적의 정렬 가능

---
+++ CHZEE93/BOJ 길라잡이 베타 (1)/b10989.py
@@ -0,0 +1,15 @@
+#백준 10989번(수 정렬하기3)
+
+import sys
+
+n = int(sys.stdin.readline().strip())
+count = [0] * 10001
+
+for _ in range(n):
+    num = int(sys.stdin.readline().strip())
+    count[num] += 1
+
+for i in range(1, 10001):
+    if count[i] > 0:
+        for _ in range(count[i]):
+            print(i)
